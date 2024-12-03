068a3689cc8b7cafeca1d7e534b6c41b229d5e3e
Zeffies <78884575+Zeffies@users.noreply.github.com>
2024-12-02 21:03:30 -0500

    added mp text
    doesn't work well on non-16:9 resolutions right now

--- Plugin.cs
+++ Plugin.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 using MonoMod.RuntimeDetour;
 using BepInEx.Configuration;
 using System.Runtime.CompilerServices;
+using System;
+using System.CodeDom;
+using System.Globalization;
 
 namespace DisplayHPStamMPHudDigits;
 
@@ -13,7 +16,13 @@ public class Plugin : BaseUnityPlugin
 {
     // variable used for if you're cursed or not
     public static Vector3 hpInfoFontText_initial = new(-1, -1, -1);
-    public static bool flagForDangerSound = false;
+    // was going to use flagForDangerSound if i made it so you could customize the % hp at which your hp flashed / had an alarm. that feature is too much work right now until i can find a better way to do it
+    // public static bool flagForDangerSound = false;
+    public static bool setupMpFont = false;
+    public static bool setupUILabel = false;
+    public static bool setupNGUILabelSetTextFromTextId = false;
+    public static bool findDiabloBar = false;
+    public static bool setupInitialColor = false;
     internal static new ManualLogSource Logger;
 
     // create config settings
@@ -26,9 +35,23 @@ public class Plugin : BaseUnityPlugin
     // trouble than it works, sadly. i'll go back and try again at some point
 
     // public static ConfigEntry<int> configHpForDanger;
+
+    // these are used for what colour your hp flashes to when less than 30% hp
     public static ConfigEntry<float> configHpDangerColourR;
     public static ConfigEntry<float> configHpDangerColourG;
     public static ConfigEntry<float> configHpDangerColourB;
+
+    // mp text color
+    public static ConfigEntry<float> configMpTextR;
+    public static ConfigEntry<float> configMpTextG;
+    public static ConfigEntry<float> configMpTextB;
+
+    // public static ConfigEntry<float> findIconSize;
+
+
+    // test var
+    public static bool testVar = false;
+
     private void Awake()
     {
         // bind settings
@@ -44,6 +67,12 @@ public class Plugin : BaseUnityPlugin
         configHpDangerColourG = Config.Bind("HpAsText.Danger", "G", (float)0, new ConfigDescription("G", new AcceptableValueRange<float>(0f, 255f)));
         configHpDangerColourB = Config.Bind("HpAsText.Danger", "B", (float)0, new ConfigDescription("B", new AcceptableValueRange<float>(0f, 255f)));
 
+        configMpTextR = Config.Bind("MpAsText.Color", "R", (float)255, new ConfigDescription("R", new AcceptableValueRange<float>(0f, 255f))); 
+        configMpTextG = Config.Bind("MpAsText.Color", "G", (float)234.6, new ConfigDescription("G", new AcceptableValueRange<float>(0f, 255f)));
+        configMpTextB = Config.Bind("MpAsText.Color", "B", (float)4.08, new ConfigDescription("B", new AcceptableValueRange<float>(0f, 255f)));
+
+        // findIconSize = Config.Bind("iconsize", "what's the icon size dude", (float)0, new ConfigDescription("B", new AcceptableValueRange<float>(0f, 100f)));
+
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
@@ -53,12 +82,179 @@ public class Plugin : BaseUnityPlugin
     }
 }
 
+[HarmonyPatch(typeof(GUIEnergyBarConnector), "Start")]
+public class GUIEnergyBarConnector_Start
+{
+    public static GameObject diabloSphereBarMp;
+    public static UILabel mpBarLabel;
+    public static NGUILabelSetTextFromTextId mpInfoFontText;
+    public static GameObject mpInfoFont;
+    public static RegistResolutionFitInfoList mpTextResolutionFix;
+
+    [HarmonyPostfix]
+    static void initializeMpBar(GUIEnergyBarConnector __instance)
+    {
+        Plugin.Logger.LogInfo("initializing mp bar text...");
+        diabloSphereBarMp = GameObject.Find("DiabloSphereBarMp");
+        if (diabloSphereBarMp != null)
+        {
+            mpInfoFont = new GameObject("mpInfoFont");
+            mpInfoFont.transform.SetParent(diabloSphereBarMp.transform);
+            // add uilabel to mp bar
+            mpBarLabel = mpInfoFont.GetComponent<UILabel>();
+            if (mpBarLabel == null)
+            {
+                Plugin.Logger.LogInfo("Adding UILabel to mpInfoFont.");
+                mpBarLabel = mpInfoFont.AddComponent<UILabel>();
+            }
+            // add nguilabel to mp bar
+            mpInfoFontText = mpInfoFont.GetComponent<NGUILabelSetTextFromTextId>();
+            if (mpInfoFontText == null)
+            {
+                Plugin.Logger.LogInfo("Adding NGUILabelSetTextFromTextId to mpInfoFont.");
+                mpInfoFontText = mpInfoFont.AddComponent<NGUILabelSetTextFromTextId>();
+            }
+            mpTextResolutionFix = mpInfoFont.GetComponent<RegistResolutionFitInfoList>();
+            if (mpTextResolutionFix == null)
+            {
+                Plugin.Logger.LogInfo("Adding RegistResolutionFitInfoList to mpInfoFont");
+                mpTextResolutionFix = mpInfoFont.AddComponent<RegistResolutionFitInfoList>();
+            }
+            if (!Plugin.setupMpFont)
+            {
+                UILabel sourceLabel = (UILabel)__instance.m_hpInfoFontText.GetComponent("UILabel");
+                UILabel targetLabel = (UILabel)mpInfoFont.GetComponent("UILabel");
+
+                targetLabel.bitmapFont = sourceLabel.bitmapFont;
+                targetLabel.fontSize = sourceLabel.fontSize;
+                targetLabel.effectStyle = sourceLabel.effectStyle;
+                targetLabel.effectColor = sourceLabel.effectColor;
+                targetLabel.effectDistance = sourceLabel.effectDistance;
+                targetLabel.alignment = sourceLabel.alignment;
+                targetLabel.overflowMethod = sourceLabel.overflowMethod;
+                targetLabel.width = sourceLabel.width;
+                targetLabel.spacingX = sourceLabel.spacingX;
+                targetLabel.spacingY = sourceLabel.spacingY;
+                targetLabel.supportEncoding = sourceLabel.supportEncoding;
+                targetLabel.symbolStyle = sourceLabel.symbolStyle;
+                Debug.Log("setting up font");
+                Plugin.setupMpFont = true;
+            }
+        }
+    }
+}
+
 [HarmonyPatch(typeof(GUIEnergyBarConnector), "Update")]
 public class GUIEnergyBarConnector_Update
 {
     [HarmonyPostfix]
     static void DisplayHPStamMPHudDigits(GUIEnergyBarConnector __instance)
     {
+        // float hpInfoFontText_Alpha = __instance.m_hpInfoFontText.GetLabelAlpha();
+        bool flag2 = __instance.m_slideAnimationMode == GUIEnergyBarConnector.SlideAnimationMode.FinishSlideIn || __instance.m_slideAnimationMode == GUIEnergyBarConnector.SlideAnimationMode.FinishSlideOut;
+
+        if (GUIEnergyBarConnector_Start.diabloSphereBarMp != null)
+        {
+
+            GUIEnergyBarConnector_Start.mpInfoFontText.SetText(((int)__instance.m_playerGos.GetMp()).ToString() + "/" + __instance.m_playerGos.m_max_mp.ToString());
+            
+
+            // copy hp text active state and do the stuff it does when active
+            if (__instance.m_hpInfoFontText.gameObject.activeSelf)
+            {
+                if (!GUIEnergyBarConnector_Start.mpInfoFont.gameObject.activeSelf)
+                {
+                    Debug.Log("setting mpinfofont active");
+                }
+                GUIEnergyBarConnector_Start.mpInfoFont.gameObject.SetActive(true);
+                if (Plugin.setupInitialColor == false)
+                {
+                    // set initial colour
+                    Debug.Log("setting up mp text initial color");
+                    GUIEnergyBarConnector_Start.mpInfoFontText.SetLabelColor(new Color (Plugin.configMpTextR.Value / 255, Plugin.configMpTextG.Value / 255, Plugin.configMpTextB.Value / 255));
+                    Plugin.setupInitialColor = true;
+                }
+
+                // copy hp text alpha
+                if (GUIEnergyBarConnector_Start.mpInfoFontText.GetLabelAlpha() != __instance.m_hpInfoFontText.GetLabelAlpha())
+                {
+                    if (__instance.m_hpInfoFontText.GetLabelAlpha() >= .01f)                                                        // todo move alpha logic to where hpinfotext processes it
+                    {
+                        // Debug.Log("mpBarLabelAlpha = " + GUIEnergyBarConnector_Start.mpInfoFontText.GetLabelAlpha());
+                        // Debug.Log("hpbar alpha= " + __instance.m_hpInfoFontText.GetLabelAlpha());
+                        GUIEnergyBarConnector_Start.mpInfoFontText.SetLabelAlpha(__instance.m_hpInfoFontText.GetLabelAlpha());
+                    }
+                    else
+                    {
+                        GUIEnergyBarConnector_Start.mpInfoFontText.SetLabelAlpha(0f);
+                    }
+                }
+                
+                // set position
+                float num = __instance.m_playerGos.GetMaxMp() / 100f;                                                                                                   //TODO move localposition logic to SetupHUDUsingPlayerGos
+                int num2 = Mathf.CeilToInt(num);
+                if (GUIEnergyBarConnector_Start.mpInfoFont.transform.localPosition != new Vector3(-874f, 385f, 1000f) + new Vector3(47.3f * num2, 0f, 0f))
+                {
+                    Debug.Log("repeat count = " + num2);
+                    GUIEnergyBarConnector_Start.mpInfoFont.transform.localPosition = new Vector3(-874f, 385f, 1000f) + new Vector3(47.3f * num2, 0f, 0f);
+                }
+                GUIEnergyBarConnector_Start.mpInfoFontText.transform.localScale = Vector3.one;
+                var currentColor = GUIEnergyBarConnector_Start.mpInfoFontText.GetLabelColor();
+                if (Math.Truncate(currentColor.r * 255) != Math.Truncate(Plugin.configMpTextR.Value) || 
+                    Math.Truncate(currentColor.g * 255) != Math.Truncate(Plugin.configMpTextG.Value) || 
+                    Math.Truncate(currentColor.b * 255) != Math.Truncate(Plugin.configMpTextB.Value)
+                    )
+                {
+                    // Debug.Log("\n current color r = " + Math.Truncate(currentColor.r * 255) + " || configmptext r =" + Math.Truncate(Plugin.configMpTextR.Value) + "\n"
+                    //         + "current color g = " + Math.Truncate(currentColor.g * 255) + " || configmptext g =" + Math.Truncate(Plugin.configMpTextG.Value)     + "\n"
+                    //         + "current color b = " + Math.Truncate(currentColor.b * 255) + " || configmptext b =" + Math.Truncate(Plugin.configMpTextB.Value));
+                    GUIEnergyBarConnector_Start.mpInfoFontText.SetLabelColor(new Color (Plugin.configMpTextR.Value / 255, Plugin.configMpTextG.Value / 255, Plugin.configMpTextB.Value / 255));
+                }
+            }
+            else if (GUIEnergyBarConnector_Start.mpInfoFont.gameObject.activeSelf)
+            {
+                Debug.Log("setting mpinfofont inactive");
+                GUIEnergyBarConnector_Start.mpInfoFont.gameObject.SetActive(false);
+            }
+
+            // if (__instance.m_hpInfoFontText.GetLabelAlpha() >= .95f && flag2)
+            // {
+            //     if (!GUIEnergyBarConnector_Start.mpInfoFont.gameObject.activeSelf)
+            //     {
+            //         Debug.Log("setting mptext as active");
+            //         GUIEnergyBarConnector_Start.mpInfoFont.gameObject.SetActive(true);
+            //     }
+            //     float num = __instance.m_playerGos.GetMaxMp() / 100f;
+            //     int num2 = Mathf.CeilToInt(num);
+            //     if (GUIEnergyBarConnector_Start.mpInfoFont.transform.localPosition != new Vector3(-864f, 385f, 1000f) + new Vector3(44f * (float)num2, 0f, 0f))
+            //     {
+            //         GUIEnergyBarConnector_Start.mpInfoFont.transform.localPosition = new Vector3(-864f, 385f, 1000f) + new Vector3(44f * (float)num2, 0f, 0f);
+            //     }
+            //     GUIEnergyBarConnector_Start.mpInfoFontText.transform.localScale = Vector3.one;
+            //     GUIEnergyBarConnector_Start.mpInfoFontText.SetLabelColor(new Color (Plugin.configMpTextR.Value / 255, Plugin.configMpTextG.Value / 255, Plugin.configMpTextB.Value / 255));
+            // }
+            // if (GUIEnergyBarConnector_Start.mpInfoFontText.GetLabelAlpha() <= 0f && GUIEnergyBarConnector_Start.mpInfoFont.gameObject.activeSelf)
+            // {
+            //     Debug.Log("setting mptext as inactive");
+            //     GUIEnergyBarConnector_Start.mpInfoFont.gameObject.SetActive(false);
+            // }
+            // if (GUIEnergyBarConnector_Start.mpInfoFont.activeSelf)
+            // {
+            //     if (GUIEnergyBarConnector_Start.mpInfoFontText.GetLabelAlpha() != __instance.m_hpInfoFontText.GetLabelAlpha())
+            //     {
+            //         if (__instance.m_hpInfoFontText.GetLabelAlpha() >= .01f)
+            //         {
+            //             Debug.Log("mpBarLabelAlpha = " + GUIEnergyBarConnector_Start.mpInfoFontText.GetLabelAlpha());
+            //             GUIEnergyBarConnector_Start.mpInfoFontText.SetLabelAlpha(__instance.m_hpInfoFontText.GetLabelAlpha());
+            //         }
+            //         else
+            //         {
+            //             GUIEnergyBarConnector_Start.mpInfoFontText.SetLabelAlpha(0f);
+            //         }
+            //     }
+            // }
+        }
+        
         // fix hp and mp bar to actually display as full if you don't have a max hp/mp stat divisible by 100
         if (Plugin.configFixHpMpFullness.Value)
         {
@@ -91,6 +287,9 @@ public class GUIEnergyBarConnector_Update
         // set hpInfoFontText to say current/max hp instead of FULL
         __instance.m_hpInfoFontText.SetText(((int)__instance.m_playerGos.GetHp()).ToString() + "/" + __instance.m_playerGos.m_max_hp.ToString());
 
+        // same for mp
+        // mystuff.mpAsText.SetText(((int)__instance.m_playerGos.GetMp()).ToString() + "/" + __instance.m_playerGos.m_max_mp.ToString());
+
         // set tween colors
         if (__instance.m_hpInfoFontTextTweenColor.from != new Color(Plugin.configHpDangerColourB.Value / 255, Plugin.configHpDangerColourG.Value / 255, Plugin.configHpDangerColourB.Value / 255) || 
             __instance.m_hpInfoFontTextTweenColor.to != new Color(Plugin.configHpTextR.Value / 255, Plugin.configHpTextG.Value / 255, Plugin.configHpTextB.Value / 255))
@@ -98,8 +297,6 @@ public class GUIEnergyBarConnector_Update
             __instance.m_hpInfoFontTextTweenColor.from = new Color(Plugin.configHpDangerColourR.Value / 255, Plugin.configHpDangerColourG.Value / 255, Plugin.configHpDangerColourB.Value / 255);
             __instance.m_hpInfoFontTextTweenColor.to = new Color(Plugin.configHpTextR.Value / 255, Plugin.configHpTextG.Value / 255, Plugin.configHpTextB.Value / 255);
         }
-
-        bool flag2 = __instance.m_slideAnimationMode == GUIEnergyBarConnector.SlideAnimationMode.FinishSlideIn || __instance.m_slideAnimationMode == GUIEnergyBarConnector.SlideAnimationMode.FinishSlideOut;
 
         // IC has a really weird way of handling whether or not to render the hpInfoFontText where it will count from 0 to 1 in decimal
         // and when it reaches 1, it changes whether you can see it or not. however, whatever function that is counting from 0 to 1
--- obj/Debug/net35/DisplayHPStamMPHudDigits.AssemblyInfo.cs
+++ obj/Debug/net35/DisplayHPStamMPHudDigits.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisplayHPStamMPHudDigits")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+6544284ae3a7d79a5b2e7416ebd194ee005a7da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+ad7d208cfa9bfbdfa1a1b0bb2d7f4c85d125e880")]
 [assembly: System.Reflection.AssemblyProductAttribute("Display HP Stam MP Hud Digits")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisplayHPStamMPHudDigits")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
